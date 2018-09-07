27652769cf4921a4d8fd5d6f7c2e552f3743d56d
Vicky Banh <samsokhua1@gmail.com>
2018-09-06 23:40:33 -0400

    Adding Error Handler to handle any type of errors

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');  // log infor of req
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
 // only req url start with /products path, will then be handled by the specific routers
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+// if not req path match the above route
+app.use((req, res, next) => {
+  const error = new Error("No found");
+  error.status = 404;
+  next(error);  // forward the error obj to the next middlewares
+});
+
+// catch all types of error that could be from the above or throw by database or other applications
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
 
 module.exports = app;
