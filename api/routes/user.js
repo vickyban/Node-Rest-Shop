9d29f5fec8abce95729e1bf1175bdbec9827fe2e
Vicky Banh <samsokhua1@gmail.com>
2018-09-08 23:23:02 -0400

    compare raw pass with hash pass to validate, and use jsonwebtoken to generate token

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const secret_key = require('../../config').SECRET_KEY;
 
 const User = require('../models/user');
 
@@ -33,6 +36,36 @@ router.post('/signup', (req, res, next) => {
       })
     })
 })
+
+router.post('/login', (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then(users => {
+      if (users.length < 1)  // no user exist
+        return res.status(401).json({ message: 'Auth failed' })    // dont want to say with email exist => then attacker can try multiple time to look for existing email
+      bcrypt.compare(req.body.password, users[0].password, (err, result) => {
+        if (err)
+          return res.status(401).json({ message: 'Auth failed' })
+        if (result) {   // true if same , false otherwise
+          const token = jwt.sign({
+            email: users[0].email,
+            userId: users[0]._id
+          }, secret_key,
+            {
+              expiresIn: '1h'
+            }
+          )
+          return res.status(200).json({
+            message: 'Auth successful',
+            token: token
+          })
+        }
+        res.status(401).json({ message: 'Auth failed' })
+      })
+    })
+    .catch()
+})
+
 
 router.delete('/id', (req, res, next) => {
   User.remove({ _id: req.params.id })
